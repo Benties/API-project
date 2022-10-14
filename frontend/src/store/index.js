e929291d593b301c3c413030e5c8db219b6c00f6
Benjamin Thai <ben.thai@aol.com>
2022-10-14 01:10:53 -0400

    csrf added

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,6 +1,8 @@
 import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 
+
+
 const rootReducer = combineReducers({
   // add reducer functions here
 });
@@ -16,8 +18,10 @@ if (process.env.NODE_ENV === "production") {
   enhancer = composeEnhancers(applyMiddleware(thunk, logger));
 }
 
+
 const configureStore = (preloadedState) => {
   return createStore(rootReducer, preloadedState, enhancer);
 };
+
 
 export default configureStore;
