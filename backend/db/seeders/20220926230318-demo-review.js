679045ae210082d7d589bff567883a2c01c09e5b
Benjamin Thai <ben.thai@aol.com>
2022-12-22 15:48:34 -0500

    fixed descriptions

--- backend/db/seeders/20220926230318-demo-review.js
+++ backend/db/seeders/20220926230318-demo-review.js
@@ -18,39 +18,33 @@ module.exports = {
       {
         spotId: 1,
         userId: 2,
-        review: `I recently stayed at this Airbnb in New York City and it was absolutely wonderful!
-                The location was perfect - just a short walk from all of the main attractions and the
-                apartment itself was clean, spacious, and had everything we needed for a comfortable stay.
-                The host was also very responsive and helpful with any questions we had. I would
-                highly recommend this Airbnb to anyone visiting NYC.`,
+        review: `This Airbnb was the perfect spot for our NYC trip.
+                The location was unbeatable, the apartment was clean and
+                comfortable, and the host was so welcoming and helpful.
+                Highly recommend!`,
         stars: 5
       },
       {
         spotId: 1,
         userId: 3,
-        review: `We had a fantastic stay at this Airbnb in the heart of Manhattan.
-                The apartment was small but perfectly appointed, with everything we needed for a comfortable stay. The location was unbeatable just a
-                few blocks from Central Park and all the best restaurants and shops. Our host, was incredibly helpful and provided us with all
-                the information we needed to make the most of our trip. We would definitely stay here again on our next visit to the city!`,
+        review: `Absolutely amazing stay in NYC! The apartment was clean, spacious, and in a
+                great location. The host was super helpful and responsive. Would highly recommend this Airbnb.`,
         stars: 5
       },
       {
         spotId: 2,
         userId: 3,
-        review: `I recently stayed at this Airbnb in New York City and it was absolutely wonderful!
-                The location was perfect - just a short walk from all of the main attractions and the
-                apartment itself was clean, spacious, and had everything we needed for a comfortable stay.
-                The host was also very responsive and helpful with any questions we had. I would
-                highly recommend this Airbnb to anyone visiting NYC.`,
+        review: `This Airbnb was the perfect spot for our NYC trip.
+                The location was unbeatable, the apartment was clean and comfortable,
+                and the host was so welcoming and helpful. Highly recommend!`,
         stars: 5
       },
       {
         spotId: 2,
         userId: 4,
-        review:`We had a fantastic stay at this Airbnb in the heart of Manhattan.
-                The apartment was small but perfectly appointed, with everything we needed for a comfortable stay. The location was unbeatable just a
-                few blocks from Central Park and all the best restaurants and shops. Our host, was incredibly helpful and provided us with all
-                the information we needed to make the most of our trip. We would definitely stay here again on our next visit to the city!`,
+        review:`Absolutely amazing stay in NYC! The apartment was clean, spacious,
+                and in a great location. The host was super helpful and responsive.
+                Would highly recommend this Airbnb.`,
         stars: 5
       },
     ])
